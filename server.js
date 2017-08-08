7dbbbd2e6b90ee59191472074a6454f62325cba6
santhooshkumarg <30746375+santhooshkumarg@users.noreply.github.com>
2017-08-08 09:30:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'santhosh.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
